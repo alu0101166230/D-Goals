f91460f129b7657c6bbd0f96bb359b2ac17a9709
k1k4ss0 <predatoren1@gmail.com>
2021-06-17 09:39:45 +0100

    disposicion del nuevo usuario hecho

--- D-Goals/src/app/new-user/new-user.component.ts
+++ D-Goals/src/app/new-user/new-user.component.ts
@@ -15,7 +15,8 @@ export class NewUserComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const tabla = document.getElementById("tabla");
+    const izquierdo = document.getElementById("grid-izquierdo");
+    const derecho = document.getElementById("grid-derecho");
 
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })}
@@ -23,33 +24,57 @@ export class NewUserComponent implements OnInit {
     this.http.get<any>(`http://10.6.130.59:8081/habit`,httpOptions).subscribe(data =>{
       console.log(data);
       for(var x=0; x< data.length; x++){
-        var tr = document.createElement("tr");
-        var th_name= document.createElement("th");
-        var th_description= document.createElement("th");
-        var th_img=document.createElement("th");
+          var container = document.createElement("div");
+          container.classList.add("container");
 
-        // tr.onclick =this.seleccion(tr);
-        tr.addEventListener("click",this.seleccion,false);
 
-        let element = data[x];
-        th_name.textContent =element["nombre"];
-        th_description.textContent = element["descripcion"];
-        th_img.textContent= "asdfasdf"; 
-        tr.appendChild(th_name);
-        tr.appendChild(th_description);
-        tr.appendChild(th_img);
-        tabla?.appendChild(tr);
+          var div_imagen = document.createElement("div");
+          div_imagen.classList.add("text-center");
+          var imagen = document.createElement("img");
+          imagen.src="../../assets/Square_200x200.png";
+          imagen.classList.add("rounded");
 
+          var div_titulo = document.createElement("div");
+          var titulo = document.createElement("h1");
+          let element = data[x];
+          titulo.textContent = element["nombre"];
+          titulo.classList.add("titulo");
+
+
+          var div_descripcion = document.createElement("div");
+          var descripcion = document.createElement("p");
+          descripcion.textContent=element["descripcion"];
+          descripcion.classList.add("descripcion");
+
+          div_descripcion.appendChild(descripcion);
+          div_titulo.appendChild(titulo);
+          div_imagen.appendChild(imagen);
+
+          
+          container.appendChild(div_imagen);
+          container.appendChild(div_titulo);
+          container.appendChild(div_descripcion);
+
+
+        if(x%2==0){
+          izquierdo?.appendChild(container);
+        }
+        if(x%2==1){
+          derecho?.appendChild(container);
+        }
+        container.addEventListener("click",this.seleccion,false);
       };
       return data;
     })
   }
   
   seleccion(){
-    console.log(this)
-    // this.habito_elegido=this.childNodes[0].textContent;
-    // Object.getPrototypeOf(this).route
-    // NewUserComponent.router.navigate(["/newhabit"]);
+    var titulo = this.getElementsByClassName("titulo")[0];
+    console.log(titulo.textContent)
+    
+    window.localStorage.setItem("habito",titulo.textContent);
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'))
+    myModal.show()
   }
 
 }
