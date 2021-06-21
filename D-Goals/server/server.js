d2b91c8853a26ff1c2811c6e2c6410718069c5d1
k1k4ss0 <predatoren1@gmail.com>
2021-06-21 11:08:12 +0100

    arreglando server

--- D-Goals/server/server.js
+++ D-Goals/server/server.js
@@ -100,30 +100,6 @@ app.post("/usuario",(req,res)=>{
 
 });
 
-$addFields
-app.post("/usuario",(req,res)=>{
-  let user = req.body.user;
-  let habit =req.body.habit;
-
-  console.log(habit);
-  // hacemos la consulta segun el usuario y la pass, retornamos el perfil
-  Usuario.find({nombre:user}).then((lists)=>{
-    res.send(lists);
-    let cuenta = lists[0];
-    Usuario.updateOne ({$addFields:{habito: habit}},function(err,res){
-     if (err) throw err;
-         console.log("1 document updated");
-         db.close();
-
-    });
-    res.send();
-
-  }).catch((e)=>{
-    res.send(e);
-  });
-
-})
-
 app.post("/singin",(req,res)=>{
   // Creamos el objeto de perfil nuevo, conforme a la informacion proporcionada por el formulario html 
   let newUser = new Usuario({
@@ -132,8 +108,8 @@ app.post("/singin",(req,res)=>{
     correo : req.body.email,
     schema_version:1.1,
     habito: {
-	vacio:true
-	}
+      vacio:true
+    }
   });
   // Escribimos en la Base de datos el nuevo perfil
   newUser.save().then((listDoc)=>{
--- D-Goals/src/app/calendario/calendario.component.ts
+++ D-Goals/src/app/calendario/calendario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { create } from 'domain';
 
 @Component({
   selector: 'app-calendario',
@@ -60,22 +61,46 @@ export class CalendarioComponent implements OnInit {
 
             //creating individual cells, filing them up with data.
             for (let j = 0; j < 7; j++) {
+                
                 if (i === 0 && j < firstDay) {
                     let cell = document.createElement("td");
                     let cellText = document.createTextNode("");
                     cell.appendChild(cellText);
                     row.appendChild(cell);
+                    let indicador =document.createElement("div");
+                    switch(j){
+                        case 0:{indicador.classList.add("Domingo");cell.appendChild(indicador);break;}
+                        case 1:{indicador.classList.add("Lunes");cell.appendChild(indicador);break;}
+                        case 2:{indicador.classList.add("Martes");cell.appendChild(indicador);break;}
+                        case 3:{indicador.classList.add("Miercoles");cell.appendChild(indicador);break;}
+                        case 4:{indicador.classList.add("Jueves");cell.appendChild(indicador);break;}
+                        case 5:{indicador.classList.add("Viernes");cell.appendChild(indicador);break;}
+                        case 6:{indicador.classList.add("Sabado");cell.appendChild(indicador);break;}
+                    }
                 }
                 else if (date > daysInMonth) {
                     break;
                 }
 
                 else {
+                    
                     let cell = document.createElement("td");
                     let cellText = document.createTextNode(date);
+                    let indicador =document.createElement("div");
+                    switch(j){
+                        case 0:{indicador.classList.add("Domingo");cell.appendChild(indicador);break;}
+                        case 1:{indicador.classList.add("Lunes");cell.appendChild(indicador);break;}
+                        case 2:{indicador.classList.add("Martes");cell.appendChild(indicador);break;}
+                        case 3:{indicador.classList.add("Miercoles");cell.appendChild(indicador);break;}
+                        case 4:{indicador.classList.add("Jueves");cell.appendChild(indicador);break;}
+                        case 5:{indicador.classList.add("Viernes");cell.appendChild(indicador);break;}
+                        case 6:{indicador.classList.add("Sabado");cell.appendChild(indicador);break;}
+                    }
+                    
                     if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                         cell.classList.add("bg-info");
-                    } // color today's date
+                    }
+                     // color today's date
                     cell.appendChild(cellText);
                     row.appendChild(cell);
                     date++;
