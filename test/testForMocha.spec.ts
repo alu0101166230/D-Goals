69df0057af046af9d7e0725a6e39dfb8444bb6dc
Leonardo Dorta <alu0101166230@ull.edu.es>
2021-08-20 13:12:58 +0100

    cambio de opciones en target y module, asi como las opciones de exclude en el tsconfig.json

--- test/testForMocha.spec.ts
+++ test/testForMocha.spec.ts
@@ -1,7 +1,9 @@
 import * as mocha from "mocha";
-var expect = require('expect');
-// import { expect } from "chai";
-import { add } from "../src/mocha-test-example";
+import * as chai from "chai";
+const expect = chai.expect
+import * as example from '../src/mocha-test-example';
+
+const add = example.add
 
 describe("add function tests", () => {
   it("add(1, 8) returns value 9", () => {
@@ -10,13 +12,5 @@ describe("add function tests", () => {
 
   it("add(-1, 8) returns value 7", () => {
     expect(add(-1, 8)).to.be.equal(7);
-  });
-
-  it("add(1.2, 3.5) returns value 4.7", () => {
-    expect(add(1.2, 3.5)).to.be.equal(4.7);
-  });
-
-  it("Not to be", () => {
-    expect(add(5, 4)).to.be.not.equal(8);
   });
 });
