89560ae4bbfa2cd800c1f52234ee0a9d5c7c4fdb
Leonardo Dorta <alu0101166230@ull.edu.es>
2021-09-06 18:18:53 +0100

    cambiando el puerto a 8080 y actualizando ruta

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/d-goals'));
+app.use(express.static(__dirname + '/../dist/D-Goals'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/d-goals/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/D-Goals/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
