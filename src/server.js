cf0787dda8820f450579ae9eae0fa3651e131d1d
Carolina Alvarez <alu0100944723@ull.edu.es>
2021-09-06 17:10:26 +0100

    Solucionando problemas con heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('../dist/d-goals'));
+app.use(express.static('../dist/d-goals/ng-blog'));
 app.get('/',function(req,res){
-    res.sendFile(path.join('../dist/d-goals/index.html'));
+    res.sendFile(path.join('../dist/d-goals/ng-blog/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
