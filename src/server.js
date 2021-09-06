a9dda250cb2b36a90d5207f6cb5c16f9e6996f6f
Leonardo Dorta <alu0101166230@ull.edu.es>
2021-09-06 12:29:23 +0100

    configurando direcciones de la carpeta dist en el server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static('../dist/D-Goals'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join('../dist/D-Goals/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
