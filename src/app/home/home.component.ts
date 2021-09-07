ab339bbad9634f3f40a5c7fa7a6ff64f8422c793
Carolina Alvarez <alu0100944723@ull.edu.es>
2021-09-07 01:56:37 +0100

    Arreglando problemas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -136,7 +136,7 @@ export class HomeComponent implements OnInit {
         cambio: "habito",
         valor:data[0]["habito"]
       });
-      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data);
+      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data=>{});
 
       data[0]["numero_de_horas_totales"]+=tiempo_total;
       datos =JSON.stringify({
@@ -144,7 +144,7 @@ export class HomeComponent implements OnInit {
         cambio: "numero_de_horas_totales",
         valor:data[0]["numero_de_horas_totales"]
       });
-      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data);
+      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data=>{});
 
       data[0]["tareas_cumplidas"]+=1;
       datos =JSON.stringify({
@@ -152,7 +152,7 @@ export class HomeComponent implements OnInit {
         cambio: "tareas_cumplidas",
         valor:data[0]["tareas_cumplidas"]
       });
-      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data);
+      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data=>{});
       
       data[0]["racha"]+=1;
       datos =JSON.stringify({
@@ -160,7 +160,7 @@ export class HomeComponent implements OnInit {
         cambio: "racha",
         valor:data[0]["racha"]
       });
-      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data);
+      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data=>{});
       
     }, error => {
         console.log(JSON.stringify(error.json()));
@@ -186,7 +186,7 @@ export class HomeComponent implements OnInit {
         cambio: "racha",
         valor:racha_broken
       });
-      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data);
+      this.http.post<any>(`api/update_usuario`,datos,httpOptions).subscribe(data=>{});
 
     }, error => {
         console.log(JSON.stringify(error.json()));
